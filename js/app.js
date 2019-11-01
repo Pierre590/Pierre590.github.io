76c05926634e8485ed20dd5a10f1e3e5841c0513
Pierre Berengue <nanashi4781@gmail.com>
2019-11-01 15:25:35 +0100

    cv

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
-$ ("#test-id").click (function() {
-$(this).css("color","red");
-})
 
+$(".button1").click(function(){
+$(".bg-info1").toggle();
+});
+
+$(".button2").click(function(){
+$(".bg-info2").toggle();
+});
+    
+$(".button3").click(function(){
+$(".bg-info3").toggle();
+});
